119d2b54ffba6f17b39a217c55c2bf3b053268aa
hyuntaiking <hyuntaiking@gmail.com>
2017-06-16 09:25:22 +0800

    0612

--- hyuntai0612/myGuessNum.swift
+++ hyuntai0612/myGuessNum.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+func myGuessNum() {
+    // 產生謎底
+    func createAnswer(length: Int) -> String {
+        var result = ""
+        var balls = Set<Int>()
+        while balls.count < length {
+            balls.insert(Int(arc4random_uniform(10)))
+        }
+        for ball in balls {
+            result.append(String(ball))
+        }
+        return result
+    }
+    
+    // 比對
+//    func checkAB(answer: String, guess: String) -> (A: Int, B: Int) 
+    func checkAB(answer: String, guess: String) -> String {
+        var a = 0, b = 0
+        var ca: String, cg: String
+        for i in 0..<answer.characters.count {
+            ca = substring(str: answer, start: i, len: 1)
+            cg = substring(str: guess, start: i, len: 1)
+            if ca == cg {
+                a += 1
+            } else if answer.contains(cg) {
+                b += 1
+            }
+        }
+        return "\(a)A\(b)B"
+    }
+    // 自制的substring
+    func substring(str: String, start: Int, len: Int) -> String {
+        // 3.再做堅固一定
+        let sindex = str.index(str.startIndex, offsetBy: start)
+        let eindex = str.index(str.startIndex, offsetBy: start + len)
+        let range = sindex..<eindex
+        return str[range]
+    }
+
+    // main flow
+    let args = CommandLine.arguments // [String]
+    // 2.強化未輸入
+    let len = Int(args[1])
+    let answer = createAnswer(length: len ?? 3)
+    
+    var guess: String?
+    var isWin: Bool = false
+    for i in 1...10 {
+        print("\(i). 請猜一個數字(\(len)位):", terminator: "")
+        guess = readLine()
+        // 檢查機制 1.Homework
+        // 1.[0-9]{len!}
+        // 2.任一數字不可重複
+        if let gus = guess {
+            let result = checkAB(answer: answer, guess: gus)
+            print("檢查結果: \(result)")
+            if result == "\(len)A0B" {
+                isWin = true
+                break
+            }
+        }
+    }
+    if isWin {
+        print("WINNER")
+    } else {
+        print("Loser:\(answer)")
+    }
+}
--- hyuntai0612/myfunction.swift
+++ hyuntai0612/myfunction.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+func myfunction() {
+    let a = 10
+    func f1() {
+        //    print("f1() => \(a)")
+        let a = 3
+        print("f1() => \(a)")
+    }
+    f1()
+    print("outer => \(a)")
+    
+    func f2() -> Int {
+        return 123
+    }
+    print(f2())
+    print(f2)
+    print(type(of: f2))
+    //var f2 = 0
+    var f3 = f2()
+    print(f3)
+    var f4 = f2
+    print(type(of: f4))
+    var f5 = f4()
+    print(f5)
+    print("------------")
+    func f6() {
+        var a = 20
+        print("f6()")
+        func f7() {
+            print("f7()")
+            print("f7() => \(a)") // 一樣存取Global
+        }
+        f7()
+    }
+    f6()
+    print("-----------")
+    func f8(a: Int) {
+        print(a)
+    }
+    f8(a: 30)
+}
